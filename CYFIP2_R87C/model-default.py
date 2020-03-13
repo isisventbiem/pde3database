da6d8ac428f422c594ebc1459892f1325c131504
Isis Venturi Biembengut <isisventbiem@gmail.com>
2020-03-13 14:20:30 -0300

    adds alignment

---
+++ CYFIP2_R87C/model-default.py
@@ -0,0 +1,18 @@
+# Comparative modeling by the automodel class
+from modeller import *              # Load standard Modeller classes
+from modeller.automodel import *    # Load the automodel class
+
+log.verbose()    # request verbose output
+env = environ()  # create a new MODELLER environment to build this model in
+
+# directories for input atom files
+env.io.atom_files_directory = ['.', '../atom_files']
+
+a = automodel(env,
+              alnfile  = 'TgHDAC5.ali',     # alignment filename
+              knowns   = '3P8C',              # codes of the templates
+              sequence = 'UKNP')              # code of the target
+a.starting_model= 1                 # index of the first model
+a.ending_model  = 50                 # index of the last model
+                                    # (determines how many models to calculate)
+a.make()                            # do the actual comparative modeling
